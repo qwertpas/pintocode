54803bc2cd688fbca39f60571590ea56613a3fa4
Christopher Xu <christopher.y.x@gmail.com>
2024-03-15 13:52:22 -0500

    finallay espnow rid of no_mem err

--- esp32s3/estop/src/main.cpp
+++ esp32s3/estop/src/main.cpp
@@ -29,7 +29,7 @@ hw_timer_t *blink_timer = NULL;
 
 
 uint32_t blink_period = 100;
-uint32_t send_period = 10; // 100Hz
+uint32_t send_period = 100; // 100Hz
 
 esp_now_peer_info_t peerInfo;
 
@@ -37,21 +37,57 @@ uint8_t packet[250] = {0};
 uint8_t packet_index = 0;
 uint8_t packet_ready_flag = false;
 
+uint8_t espnow_tx[250] = {0};
+uint8_t espnow_tx_size = 0;
+
+uint8_t led_solid = false;
+
 
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-    if (status == 0) {
-        // blink_period = 0; // solid light indicates transmit success
-        timerAlarmWrite(blink_timer, 10*1000, true);
-        
+    if (status == ESP_OK) {
+        led_solid = true;
     } else {
-        // blink_period = 200; // fast blink indicates transmit fail
-        timerAlarmWrite(blink_timer, 200*1000, true); //microseconds
+        timerAlarmWrite(blink_timer, 200*1000, true); //microseconds, fast blink indicates transmit fail
+        led_solid = false;
     }
+
+    // switch(status){
+    //     case ESP_OK:
+    //         Serial.println("OK");
+    //         timerAlarmWrite(blink_timer, 1*1000, true);
+    //         led_solid = true;
+    //         break;
+    //     case ESP_ERR_ESPNOW_NOT_INIT:
+    //         Serial.println("NOT_INIT");
+    //         break;
+    //     case ESP_ERR_ESPNOW_ARG:
+    //         Serial.println("ARG");
+    //         break;
+    //     case ESP_ERR_ESPNOW_INTERNAL:
+    //         Serial.println("INTERNAL");
+    //         break;
+    //     case ESP_ERR_ESPNOW_NO_MEM:
+    //         Serial.println("NO_MEM");
+    //         break;
+    //     case ESP_ERR_ESPNOW_NOT_FOUND:
+    //         Serial.println("NOT_FOUND");
+    //         break;
+    //     case ESP_ERR_ESPNOW_IF:
+    //         Serial.println("IF");
+    //         break;
+    //     default:
+    //         break;
+    // }
 }
 
 void IRAM_ATTR blink_timer_ISR() { // 1kHz
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    if(led_solid){
+        digitalWrite(LED_BUILTIN, LED_ON);
+    }else{
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    }
 }
+
 
 void setup() {
     Serial.begin(115200);
@@ -82,118 +118,50 @@ void setup() {
     timerAlarmWrite(blink_timer, 1000000, true);                 // microseconds, true=autoreload
     timerAlarmEnable(blink_timer);                            // start counting
 
+
     Serial.println("Finished setup");
 }
 
 void loop() {
 
-    // if (blink_period == 0) {
-    //     led_timer = 0; // solid light
-    //     digitalWrite(LED_BUILTIN, LED_ON);
-    // } else if (led_timer > blink_period) {
-    //     led_timer = 0;
-    //     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    packet_ready_flag = false;
+    // while(true){
+    if(Serial.available()){
+        uint8_t newchar = Serial.read();
+        // Serial.print((char) newchar);
+        packet[packet_index] = newchar;
+        packet_index++;
+        if(newchar == '#'){
+            packet_ready_flag = true;
+
+            Serial.println("packet ready:");
+            String packet_str = String((char *)packet);
+            Serial.println(packet_str);
+
+            // break;
+        }
+    }
     // }
 
-
-
-    // if (send_timer > send_period) {
-    if (true) {
-        send_timer = 0;
-
-        // Serial.println("send loop");
-
-        // Serial.println(Serial.readStringUntil('@'));
+    if(packet_ready_flag){
         packet_ready_flag = false;
-        while(send_timer < send_period){
-            if(Serial.available()){
-                uint8_t newchar = Serial.read();
-                // Serial.print((char) newchar);
-                packet[packet_index] = newchar;
-                packet_index++;
-                if(newchar == '#'){
-                    packet_ready_flag = true;
-                    // Serial.println("");
-                    Serial.println("packet ready:");
-                    // for(int i = 0; i < 6; i++){
-                    //     Serial.print(packet[i]);
-                    // }
-                    String packet_str = String((char *)packet);
-                    Serial.println(packet_str);
-                    break;
-                }
-            }
-        }
 
-        if(packet_ready_flag){
-            
-            packet_ready_flag = false;
+        memcpy(espnow_tx, packet, packet_index);
+        espnow_tx_size = packet_index;
 
-            esp_now_send(0, packet, packet_index);
-            Serial.println("sent");
-
-            memset(packet, 0, packet_index);
-            packet_index = 0;
-
-            
-
-            // MyData myData;
-            // memset(&myData, 0, sizeof(myData));
-            // packet.toCharArray(myData.serialData, sizeof(myData.serialData));
-        }else{
-            timerAlarmWrite(blink_timer, 1000000, true); //microseconds
-        }
-
-
-        // if (Serial.available()) {
-
-        //     Serial.println("sdsd");
-        //     /**
-        //      * 1. read all of serial, put into buffer
-        //      * 2. find index of '\t'
-        //      * 3. if found '\t', send (lastbuf + newbuf[0:index]). else read serial again
-        //      * 4. lastbuf = newbuf[index:-1]
-        //      */
-
-        //     uint8_t endindex = -1;
-        //     packet_ready_flag = false;
-        //     while(true){ //make sure while loop doesn't keep going
-        //         Serial.println("reading");
-        //         String newbuf = Serial.readString();
-        //         Serial.println("done read");
-
-        //         endindex = newbuf.indexOf('\t');
-        //         if(endindex == -1){
-        //             packet += newbuf; //end of packet not found
-        //             Serial.println("still finding");
-        //         }else{
-        //             packet += newbuf.substring(0, endindex);
-        //             packet_ready_flag = true;
-        //             Serial.println("end of packet");
-        //             break;
-        //         }
-        //     }
-
-        //     Serial.println("packet");                          // try see if this is the whole packet
-        //     Serial.println(packet);                       // try see if this is the whole packet
-        //     Serial.println(packet_ready_flag);                       // try see if this is the whole packet
-
-
-        //     MyData myData;
-        //     memset(&myData, 0, sizeof(myData));
-        //     packet.toCharArray(myData.serialData, sizeof(myData.serialData));
-
-        //     esp_now_send(0, (uint8_t *)&myData, sizeof(myData));
-
-        //     // uint8_t data[] = {2,3,4,5};
-
-        //     // esp_now_send(0, data, 3);
-
-        // } else {
-        //     blink_period = 1000; // long blink for no input data
-        // }
-
-
-
+        memset(packet, 0, packet_index);
+        packet_index = 0;
+    }else{
+        timerAlarmWrite(blink_timer, 1000000, true); //microseconds
+        led_solid = false;
     }
+
+    if(send_timer > 10){
+        send_timer = 0;
+        if(espnow_tx_size > 0){
+            esp_now_send(recv_addrs[1], espnow_tx, espnow_tx_size); //don't read return message here, often no_mem errror
+        }
+        
+    }
+
 }
